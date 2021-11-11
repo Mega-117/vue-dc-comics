e43684bca44872fe6d3158bfe37391f766c58ae0
Mega-117 <enricosaian@hotmail.it>
2021-11-11 16:10:00 +0100

    inizio header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,65 @@
-<template></template>
+
+<template>
+  <header>
+    <img src="../assets/dc-logo.png" alt="" />
+    <nav>
+      <ul>
+        <li v-for="(item, i) in navBarItem" :key="i">{{ item.title }}</li>
+      </ul>
+    </nav>
+  </header>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "Header",
+  data() {
+    return {
+      navBarItem: [
+        {
+          title: "charaters",
+          href: "#",
+        },
+        {
+          title: "comics",
+          href: "#",
+        },
+        {
+          title: "movies",
+          href: "#",
+        },
+        {
+          title: "tv",
+          href: "#",
+        },
+        {
+          title: "games",
+          href: "#",
+        },
+        {
+          title: "collctibles",
+          href: "#",
+        },
+        {
+          title: "video",
+          href: "#",
+        },
+        {
+          title: "fans",
+          href: "#",
+        },
+        {
+          title: "news",
+          href: "#",
+        },
+        {
+          title: "shop",
+          href: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
